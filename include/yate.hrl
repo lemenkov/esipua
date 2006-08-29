7666e6e3d755f99f8051d43ba9d3c940a459ed8d
Mikael Magnusson <mikma@users.sourceforge.net>
2006-08-29 23:18:02 +0000

    renamed #message.processed to #command.success, and #command.retval to #message.retval
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/eryate/trunk@269 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- include/yate.hrl
+++ include/yate.hrl
@@ -1,7 +1,7 @@
--record(command,{type,id,retvalue,header,keys}).
+-record(command,{type,id,success,header,keys}).
 
 %% command header records
--record(message,{time,name,processed}).
+-record(message,{time,name,retvalue}).
 -record(install,{priority,filter}).
 -record(uninstall,{priority}).
 -record(watch,{}).
