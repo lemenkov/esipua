959d0beec23710e1aae0159d268c0586c1e4bf7a
Mikael Magnusson <mikma@users.sourceforge.net>
2006-09-30 13:53:37 +0000

    Add dummy SDP body
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/eryate/trunk@373 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- src/esipua/siptest.erl
+++ src/esipua/siptest.erl
@@ -2,6 +2,8 @@
 
 -behaviour(sipcall).
 
+-include("sdp.hrl").
+
 %% api
 -export([run/0, register/0, unregister/0]).
 
@@ -14,11 +16,27 @@ run() ->
     [From] = contact:parse(["From Name <sip:2001@skinner.hem.za.org>"]),
     [To] = contact:parse(["To Name <sip:2002@skinner.hem.za.org>"]),
 
-    {ok, Request} = sipcall:build_invite(From, To, <<>>),
+    {ok, Body} = create_sdp_body(siphost:myip(), 12345),
+
+    {ok, Request} = sipcall:build_invite(From, To, Body),
     
     {ok, Call} = sipcall:start_link(?MODULE, [], []),
     ok = sipcall:send_invite(Call, Request),
     {ok, Call}.
+
+create_sdp_body(Localip, Localport) when is_list(Localip),
+					 is_integer(Localport) ->
+    Seconds = integer_to_list(yate_util:seconds()),
+    Origin = #sdp_origin{username="-", session_id=Seconds, version=Seconds,
+			 network_type='IN', address_type='IP4',
+			 address=Localip},
+    Connection = #sdp_connection{network_type='IN', address_type='IP4',
+				 address = Localip},
+    Media = #sdp_media{media=audio, port=Localport, transport="RTP/AVP",
+		       fmts=[8, 0], connection=Connection},
+    Sdp = #sdp{origin=Origin, session_name="Yxa", media=[Media]},
+    Body = list_to_binary(lists:flatten(sdp:print(Sdp))),
+    {ok, Body}.
 
 register() ->
     Aor = get_aor(),
