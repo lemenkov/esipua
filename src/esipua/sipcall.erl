092d07032fc821f2aeb64475a486cb2910464019
Mikael Magnusson <mikma@users.sourceforge.net>
2006-09-30 23:51:53 +0000

    Add support for early dialogs, and 200 Ok received for multiple dialogs. Answer ACK and terminate with BYE
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/eryate/trunk@375 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- src/esipua/sipcall.erl
+++ src/esipua/sipcall.erl
@@ -45,7 +45,8 @@
 	 handle_info/3,
 	 terminate/3]).
 
--record(state, {dialog,				% SIP Dialog
+-record(state, {dialog,				% Final SIP Dialog
+		early_dialogs=[],		% List of early dialogs
 		module,				% Behaviour module
 		options,			% Behaviour options
 		invite_req,			% In/out INVITE request
@@ -104,7 +105,13 @@ response(Response, Origin, LogStr) when is_record(Response, response), is_record
 		{ok, Dc_pid} ->
 		    logger:log(normal, "sipclient: Response to ~s: '~p ~s', no matching transaction, matching dialog ~p - dropping", [LogStr, Status, Reason, Dc_pid]);
 		_ ->
-		    logger:log(normal, "sipclient: Response to ~s: '~p ~s', no matching transaction, no matching dialog - dropping", [LogStr, Status, Reason])
+		    CallId = sipheader:callid(Response#response.header),
+		    case callregister:find_call(CallId) of
+			{ok, Pid} ->
+			    Pid ! {new_response, Response, Origin, LogStr};
+			_ ->
+			    logger:log(normal, "sipclient: Response to ~s: '~p ~s', no matching transaction, no matching dialog, no matching call - dropping", [LogStr, Status, Reason])
+		    end
 	    end;
 	true ->
             logger:log(normal, "sipclient: Response to ~s: '~p ~s', no matching transaction - dropping",
@@ -133,13 +140,19 @@ call(From, To, Body) when is_record(From, contact),
 %%--------------------------------------------------------------------
 
 generate_new_request(Method, Dialog, Contact) ->
-    ExtraHeaders = [{"Contact",  [Contact]}],
-    {ok, Request, Dialog1, _Dst} = sipdialog:generate_new_request(Method, ExtraHeaders, <<>>, Dialog),
-    {ok, Request, Dialog1}.
+    generate_new_request(Method, Dialog, Contact, []).
 
-generate_new_request(Method, Dialog, Contact, CSeqNum) ->
-    ExtraHeaders = [{"Contact",  [Contact]}],
-    {ok, Request, Dialog1, _Dst} = sipdialog:generate_new_request(Method, ExtraHeaders, <<>>, Dialog, CSeqNum),
+generate_new_request(Method, Dialog, Contact, ExtraHeaders) when is_list(ExtraHeaders) ->
+    ExtraHeaders1 = [{"Contact",  [Contact]} | ExtraHeaders],
+    {ok, Request, Dialog1, _Dst} = sipdialog:generate_new_request(Method, ExtraHeaders1, <<>>, Dialog),
+    {ok, Request, Dialog1};
+
+generate_new_request(Method, Dialog, Contact, CSeqNum) when is_integer(CSeqNum)->
+    generate_new_request(Method, Dialog, Contact, CSeqNum, []).
+
+generate_new_request(Method, Dialog, Contact, CSeqNum, ExtraHeaders) ->
+    ExtraHeaders1 = [{"Contact",  [Contact]} | ExtraHeaders],
+    {ok, Request, Dialog1, _Dst} = sipdialog:generate_new_request(Method, ExtraHeaders1, <<>>, Dialog, CSeqNum),
     {ok, Request, Dialog1}.
 
 
@@ -288,6 +301,8 @@ handle_event(stop, _StateName, State) ->
     {stop, normal, State};
 handle_event({send_invite, Request}, start, State) ->
     [Contact] = keylist:fetch('contact', Request#request.header),
+    CallId = sipheader:callid(Request#request.header),
+    ok = callregister:register_call(CallId, self()),
     State0 = State#state{invite_req=Request, contact=Contact},
     case do_send_invite(Request, State0) of
 	{ok, State1} ->
@@ -342,11 +357,8 @@ handle_info({branch_result, Pid, Branch, BranchState, #response{status=Status}=R
 	    {next_state, StateName, State1}
     end;
 
-handle_info({new_response, #response{status=Status}=Response, Origin, _LogStr}, StateName, State) when is_record(Origin, siporigin) ->
-    %% FIXME retransmit ACK
-    logger:log(normal, "200 OK retransmit received '~p ~s' to my invite",
-	       [Status, Response#response.reason]),
-    {next_state, StateName, State};
+handle_info({new_response, Response, Origin, LogStr}, StateName, State) when is_record(Origin, siporigin) ->
+    handle_new_response(Response, Origin, LogStr, StateName, State);
 
 handle_info({new_request, FromPid, Ref, #request{method="ACK"} = _NewRequest, _Origin, _LogStrInfo}, StateName, State) ->
     %% Don't answer ACK
@@ -396,6 +408,24 @@ handle_info({retry_invite, Request}, StateName, State) ->
 	    {stop, normal, State}
     end;
 
+handle_info({dialog_expired, {CallId, LocalTag, RemoteTag}=DialogId}, StateName, State) ->
+    error_logger:info_msg("~p: Refresh dialog ~p~n", [?MODULE, DialogId]),
+    sipdialog:set_dialog_expires(CallId, LocalTag, RemoteTag, 30),
+
+%%     case compare_dialog(DialogId, State#state.dialog) of
+%% 	{ok, Dialog} ->
+%% 	    %% Update established dialog
+%% 	    sipdialog:set_dialog_expires(Dialog, 30);
+%% 	error ->
+%% 	    %% Terminate all dialogs
+%% 	    Early = State#state.early_dialogs,
+%% 	    case find_dialog(DialogId, Early) of
+%% 		{ok, Dialog1} ->
+%% 		    Early1 = drop_dialog(Dialog1, Early),
+%% 		    xxxxxxxxxxxx
+
+    {next_state, StateName, State};
+
 handle_info(Info, StateName, State) ->
     error_logger:error_msg("~p: Unhandled info in ~p ~p~n",
 			   [?MODULE, Info, StateName]),
@@ -439,16 +469,17 @@ pred_skip_resp(_BranchState, _Status, _Response, State) ->
 
 
 send_prack(Response, Rseq, State) ->
-    Dialog = set_dialog_state_uac(Response, State),
-    {ok, Request, NewDialog} = generate_new_request("PRACK", Dialog, State#state.contact),
+    {Dialog, State0} = need_dialog(Response, State),
+    {ok, Request, NewDialog} = generate_new_request("PRACK", Dialog, State0#state.contact),
 
 %%     throw({send_prack, Dialog, Request}),
 
-    Rack = lists:concat([Rseq, " ", State#state.invite_cseqno, " INVITE"]),
+    Rack = lists:concat([Rseq, " ", State0#state.invite_cseqno, " INVITE"]),
     Header1 = keylist:set("RAck", [Rack], Request#request.header),
     
     {ok, _Pid, _Branch} = siphelper:send_request(Request#request{header=Header1}),
-    State1 = State#state{dialog=NewDialog, invite_rseqno=Rseq},
+    Early = update_dialogs(NewDialog, State0#state.early_dialogs),
+    State1 = State0#state{invite_rseqno=Rseq, early_dialogs=Early},
 
     % TODO add Pid to outgoing pid list
     {ok, State1}.
@@ -457,16 +488,19 @@ send_prack(Response, Rseq, State) ->
 set_dialog_state_uac(Response, State) when is_record(Response, response),
 					   is_record(State, state) ->
     case State#state.dialog of
-	undefined ->
-	    {ok, Dialog1} =
-		sipdialog:create_dialog_state_uac(State#state.invite_req, Response),
-	    ok = sipdialog:register_dialog_controller(Dialog1, self()),
+	_ ->
+	    {ok, Dialog1} = create_dialog_state_uac(State#state.invite_req, Response),
 	    Dialog1;
 	
 	Dialog1 ->
  	    update_dialog_state_uac(Response, Dialog1)
     end.
 
+create_dialog_state_uac(Request, Response) when is_record(Request, request),
+						is_record(Response, response) ->
+    {ok, Dialog} = sipdialog:create_dialog_state_uac(Request, Response),
+    ok = sipdialog:register_dialog_controller(Dialog, self()),
+    Dialog.
 
 update_dialog_state_uac(Response, Dialog) when is_record(Response, response),
 						is_record(Dialog, dialog) ->
@@ -491,28 +525,18 @@ refresh_dialog_target_uac(Response, Dialog) when is_record(Response, response),
     Dialog#dialog{remote_target=RemoteTarget}.
 
 
-handle_invite_result(_Pid, _Branch, BranchState, #response{status=Status}=Response, StateName, State) ->
+handle_invite_result(Pid, Branch, BranchState, #response{status=Status}=Response, StateName, State) ->
     logger:log(normal, "branch_result: ~p ~p~n", [BranchState, Status]),
     if
 	%% TODO Handle all 101 <= Status <= 199
 	BranchState == proceeding, Status == 180 ->
-	    Dialog = set_dialog_state_uac(Response, State),
+	    {_Dialog, State1} = need_dialog(Response, State),
+
 	    %% TODO Send to
-	    State1 = State#state{dialog=Dialog},
             {next_state, StateName, State1};
         BranchState == terminated, Status >= 200, Status =< 299 ->
 	    logger:log(normal, "Answered dialog: ~p ~p", [BranchState, Status]),
-	    Dialog = set_dialog_state_uac(Response, State),
-
-	    %% TODO send answered
-	    {ok, Ack, Dialog1} =
-		generate_new_request("ACK", Dialog, State#state.contact,
-				     State#state.invite_cseqno),
-	    {ok, _SendingSocket, _Dst, _TLBranch} = send_ack(Ack),
-	    State1 = State#state{dialog=Dialog1},
-	    {next_state, up, State1};
-
-%%  	    {next_state, StateName, State};
+	    handle_invite_2xx(Pid, Branch, BranchState, Response, StateName, State);
 	BranchState == completed, Status >= 300, Status =< 399 ->
             {stop, {siperror, Status, Response#response.reason}, State};
 
@@ -548,12 +572,65 @@ handle_invite_result(_Pid, _Branch, BranchState, #response{status=Status}=Respon
     end.
 
 
+handle_invite_2xx(_Pid, _Branch, _BranchState, Response, outgoing=_StateName, State) ->
+    Early = State#state.early_dialogs,
+    Dialog =
+	case find_dialog(Response, Early) of
+	    {ok, Dialog2} ->
+		update_dialog_state_uac(Response, Dialog2);
+	    error ->
+		create_dialog_state_uac(State#state.invite_req, Response)
+	end,
 
-send_ack(Request) ->
+    Early1 = drop_dialog(Dialog, Early),
+
+    %% FIXME wait 64T1 and drop early dialogs
+    %% TODO send answered
+    {ok, Ack, Dialog1} =
+	generate_new_request("ACK", Dialog, State#state.contact,
+			     State#state.invite_cseqno),
+    {ok, _SendingSocket, _Dst, _TLBranch} = send_ack(Ack, State#state.auths),
+    State1 = State#state{dialog=Dialog1, early_dialogs=Early1},
+    {next_state, up, State1}.
+
+handle_new_response(#response{status=Status}=Response, _Origin, _LogStr, up=StateName, State) ->
+    %% FIXME retransmit ACK
+    logger:log(normal, "200 OK received '~p ~s' to my invite",
+	       [Status, Response#response.reason]),
+    %% Acknowledge and terminate dialog
+
+    Early = State#state.early_dialogs,
+    Dialog =
+	case find_dialog(Response, Early) of
+	    {ok, Dialog2} ->
+		update_dialog_state_uac(Response, Dialog2);
+	    error ->
+		create_dialog_state_uac(State#state.invite_req, Response)
+	end,
+
+    Early1 = drop_dialog(Dialog, Early),
+
+    {ok, Ack, Dialog1} =
+	generate_new_request("ACK", Dialog, State#state.contact,
+			     State#state.invite_cseqno),
+    {ok, _SendingSocket, _Dst, _TLBranch} = send_ack(Ack, State#state.auths),
+
+    ExtraHeaders = [{"Reason", ["SIP ;cause=200 ;text=\"Call completed elsewhere\""]}],
+    {ok, Bye, Dialog3} =
+	generate_new_request("BYE", Dialog1, State#state.contact, ExtraHeaders),
+    {ok, _Pid, _Branch} = siphelper:send_request(Bye),
+
+    ok = sipdialog:unregister_dialog_controller(Dialog3),
+
+    State1 = State#state{early_dialogs=Early1},
+    {next_state, StateName, State1}.
+
+
+send_ack(Request, Auths) ->
     Branch = siprequest:generate_branch(),
-    send_ack(Request, Branch).
+    send_ack(Request, Auths, Branch).
 
-send_ack(Request, Branch) ->
+send_ack(Request, _Auths, Branch) ->
     Route = keylist:fetch('route', Request#request.header),
     TargetURI = Request#request.uri,
     Dst = case Route of
@@ -567,7 +644,11 @@ send_ack(Request, Branch) ->
 		  Dst1
 	  end,
 
-    case transportlayer:send_proxy_request(none, Request, Dst,
+    %% FIXME Should use request uri from INVITE
+    %%{ok, Request1} = siphelper:add_authorization(Request, Auths),
+    Request1 = Request,
+
+    case transportlayer:send_proxy_request(none, Request1, Dst,
 					   ["branch=" ++ Branch]) of
 	{ok, SendingSocket, TLBranch} ->
 	    {ok, SendingSocket, Dst, TLBranch};
@@ -599,3 +680,83 @@ do_send_invite(Request, State) ->
 	R ->
 	    R
     end.
+
+
+need_dialog(Response, State) ->
+    Early = State#state.early_dialogs,
+    case find_dialog(Response, Early) of
+	{ok, Dialog1} ->
+	    %% Ignore
+	    logger:log(normal, "Early dialog exists, ignore", []),
+	    {Dialog1, State};
+	error ->
+	    Dialog1 = create_dialog_state_uac(State#state.invite_req, Response),
+	    Early1 = add_dialog(Dialog1, Early),
+	    
+	    State1 = State#state{early_dialogs=Early1},
+	    {Dialog1, State1}
+    end.
+
+
+update_dialogs(Dialog, Dialogs) when is_record(Dialog, dialog),
+				     is_list(Dialogs) ->
+    Update = fun(D) when is_record(D, dialog) ->
+		     case compare_dialog(Dialog, D) of
+			 true ->
+			     Dialog;
+			 false ->
+			     D
+		     end
+	     end,
+
+    lists:map(Update, Dialogs).
+
+
+drop_dialog(Dialog, Dialogs) when is_record(Dialog, dialog),
+				  is_list(Dialogs) ->
+    lists:filter(fun (D) ->
+			 not compare_dialog(D, Dialog)
+		 end, Dialogs).
+
+
+find_dialog(_DialogId, []) ->
+    error;
+
+find_dialog(Response, Dialogs) when is_record(Response, response),
+				     is_list(Dialogs) ->
+    Header = Response#response.header,
+    CallId = sipheader:callid(Header),
+
+    LocalTag = sipheader:get_tag(keylist:fetch('from', Header)),
+    RemoteTag = sipheader:get_tag(keylist:fetch('to', Header)),
+    find_dialog({CallId, LocalTag, RemoteTag}, Dialogs);
+
+find_dialog({CallId, LocalTag, RemoteTag}=DialogId, [Dialog|R]) when is_list(CallId),
+			     is_list(LocalTag),
+			     is_list(RemoteTag),
+			     is_record(Dialog, dialog) ->
+
+    case compare_dialog(DialogId, Dialog) of
+	true ->
+	    {ok, Dialog};
+	false ->
+	    find_dialog(DialogId, R)
+    end.
+
+add_dialog(Dialog, Dialogs) when is_record(Dialog, dialog),
+				 is_list(Dialogs) ->
+    [Dialog | Dialogs].
+
+compare_dialog(D1, D2) when is_record(D1, dialog),
+			    is_record(D2, dialog) ->
+    D1#dialog.callid == D2#dialog.callid andalso
+	D1#dialog.local_tag == D2#dialog.local_tag andalso
+	D1#dialog.remote_tag == D2#dialog.remote_tag;
+
+compare_dialog({CallId, LocalTag, RemoteTag}, D2) when is_list(CallId),
+						       is_list(LocalTag),
+						       is_list(RemoteTag),
+						       is_record(D2, dialog) ->
+    CallId == D2#dialog.callid andalso
+	LocalTag == D2#dialog.local_tag andalso
+	RemoteTag == D2#dialog.remote_tag.
