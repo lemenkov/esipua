01851e5345167dfab7c64b2dfb336af181f698c3
Mikael Magnusson <mikma@users.sourceforge.net>
2006-10-30 20:46:25 +0000

    Add erl sources to EXTRA_DIST
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/eryate/trunk@550 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- rules.mk
+++ rules.mk
@@ -5,7 +5,7 @@ ebindir = $(libsubdir)/ebin
 beam_FILES = $($(OPT_APP)_SRCS:.erl=.beam)
 
 ebin_DATA = $(beam_FILES) $(OPT_APP:=.app) $(OPT_APP:=.boot)
-EXTRA_DIST = $(erl_FILES) $(OPT_APP:=.app-in) $(OPT_APP:=.rel)
+EXTRA_DIST = $($(OPT_APP)_SRCS) $(OPT_APP:=.app-in) $(OPT_APP:=.rel)
 CLEANFILES = $(beam_FILES) $(OPT_APP:=.app) $(OPT_APP:=.boot)	\
 $(OPT_APP:=.script)
 
