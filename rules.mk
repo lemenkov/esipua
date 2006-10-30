d2e9282e516e6ab62fb8da6b32fda738f3710137
Mikael Magnusson <mikma@users.sourceforge.net>
2006-10-30 19:21:33 +0000

    Moved Erlang make rules to rules.mk
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/eryate/trunk@544 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

---
+++ rules.mk
@@ -0,0 +1,19 @@
+libsubdir = $(ERLANG_INSTALL_LIB_DIR)/$(OPT_APP)-$($(OPT_APP)_VSN)
+bindir = $(libsubdir)/bin
+ebindir = $(libsubdir)/ebin
+
+beam_FILES = $($(OPT_APP)_SRCS:.erl=.beam)
+
+ebin_DATA = $(beam_FILES) $(OPT_APP:=.app) $(OPT_APP:=.boot)
+EXTRA_DIST = $(erl_FILES) $(OPT_APP:=.app-in) $(OPT_APP:=.rel)
+CLEANFILES = $(beam_FILES) $(OPT_APP:=.app) $(OPT_APP:=.boot)	\
+$(OPT_APP:=.script)
+
+%.beam: %.erl
+	$(ERLC) $(AM_ERLFLAGS) $(ERLFLAGS) $(AM_ERLCFLAGS) $(ERLCFLAGS) $<
+
+%.app: %.app-in
+	cp $< $@
+
+%.boot: %.rel %.app
+	$(ERLC) $(AM_ERLFLAGS) $(ERLFLAGS) $(AM_ERLCFLAGS) $(ERLCFLAGS) $<
