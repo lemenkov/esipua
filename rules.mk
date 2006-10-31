480a20590d085f2a5b8051ccc385086fe1c95929
Mikael Magnusson <mikma@users.sourceforge.net>
2006-10-31 09:58:18 +0000

    Use autodetected OTP library versions
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/eryate/trunk@554 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- rules.mk
+++ rules.mk
@@ -5,9 +5,17 @@ ebindir = $(libsubdir)/ebin
 beam_FILES = $($(OPT_APP)_SRCS:.erl=.beam)
 
 ebin_DATA = $(beam_FILES) $(OPT_APP:=.app) $(OPT_APP:=.boot)
-EXTRA_DIST = $($(OPT_APP)_SRCS) $(OPT_APP:=.app-in) $(OPT_APP:=.rel)
-CLEANFILES = $(beam_FILES) $(OPT_APP:=.app) $(OPT_APP:=.boot)	\
+EXTRA_DIST = $($(OPT_APP)_SRCS) $(OPT_APP:=.app-in) $(OPT_APP:=.rel.in)
+CLEANFILES = $(beam_FILES) $(OPT_APP:=.app) $(OPT_APP:=.boot) $(OPT_APP:=.rel)	\
 $(OPT_APP:=.script)
+
+SUBST = sed \
+	-e 's,[@]ERLANG_LIB_VER_kernel[@],$(ERLANG_LIB_VER_kernel),g' \
+	-e 's,[@]ERLANG_LIB_VER_stdlib[@],$(ERLANG_LIB_VER_stdlib),g' \
+	-e 's,[@]ERLANG_LIB_VER_sasl[@],$(ERLANG_LIB_VER_sasl),g'
+
+%.rel: %.rel.in
+	$(SUBST) $< > $@
 
 %.beam: %.erl
 	$(ERLC) $(AM_ERLFLAGS) $(ERLFLAGS) $(AM_ERLCFLAGS) $(ERLCFLAGS) $<
