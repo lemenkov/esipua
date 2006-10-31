84e6f0d29fa5cb64d992f6d55c82db6f51ec8e34
Mikael Magnusson <mikma@users.sourceforge.net>
2006-10-31 10:46:56 +0000

    Move headers to application source directory. Add support for installed headers using <APP>_HDRS variable in rules.mk.
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/eryate/trunk@555 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- rules.mk
+++ rules.mk
@@ -1,7 +1,9 @@
 libsubdir = $(ERLANG_INSTALL_LIB_DIR)/$(OPT_APP)-$($(OPT_APP)_VSN)
 bindir = $(libsubdir)/bin
 ebindir = $(libsubdir)/ebin
+incdir = $(libsubdir)/include
 
+inc_HEADERS = $($(OPT_APP)_HDRS)
 beam_FILES = $($(OPT_APP)_SRCS:.erl=.beam)
 
 ebin_DATA = $(beam_FILES) $(OPT_APP:=.app) $(OPT_APP:=.boot)
